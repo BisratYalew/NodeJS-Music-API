2201958b89f9a0dba754215ca97ac802a512fcc9
Bisrat Yalew <bisratyalew10@gmail.com>
2017-12-22 05:14:47 -0800

    Update music.js

--- models/music.js
+++ models/music.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 // Music Schema
 var MusicSchema = new mongoose.Schema({
     title: 	{ type: String, required: true },
-	artist:    [{ type: mongoose.Schema.Types.ObjectId, default: 'Unknown Artist', ref: 'Artist' }],
+    artist:    [{ type: mongoose.Schema.Types.ObjectId, default: 'Unknown Artist', ref: 'Artist' }],
     album: [{ type: mongoose.Schema.Types.ObjectId, default: 'Unknown Album', ref: 'Album' }],
     date_created: { type: Date },
     last_modified: { type: Date },
-	role: { type: String, default: 'readWrite' }
+    role: { type: String, default: 'readWrite' }
 
 }, {timestamps: { createdAt: 'date_created', updatedAt: 'last_modified' } });
 
