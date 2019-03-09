ab9ced7333da26cb1444b08c15c2e21fcaf3e8d0
bisratyalew <bisratyalew10@gmail.com>
2019-03-09 08:37:22 -0800

    Removed unnecessary route

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-// Load Module Dependencies
-// 1. Core Modules
-// 2. Third Party Modules
-// 3. Local Modules
+var express 		= require('express');
+var bodyParser 		= require('body-parser');
+var lodash      	= require('lodash'); 
+var mongoose		= require('mongoose');
+var validator 		= require('express-validator'); // Express Validator
 
-var express 	= require('express');
-var bodyParser 	= require('body-parser');
-var lodash      = require('lodash'); 
-var mongoose	= require('mongoose');
-var validator 	= require('express-validator'); // Express Validator
-
-var config 			 = require('./config');
-var logger			 = require('./lib/logger');
-var router           = require('./routes');
+var config 			= require('./config');
+var logger			= require('./lib/logger');
+var router          = require('./routes');
 
 
-var app = express();
+var app 			= express();
 
-var getRoot = router.getRoot;
+var getRoot 		= router.getRoot;
 
-// Listen on Port
+// Start Server
 app.listen(config.HTTP_PORT, function connectionListener(){
-	console.log('Blog API Server Running on Port ', config.HTTP_PORT);
+	console.log('API Server running on ', config.HTTP_PORT);
 });
 
 //Connect to MongoDB
@@ -33,17 +28,10 @@ mongoose.connect(config.MongoURL, function(err, data){
 	}
 });
 
-app.get('/', function getRoot(req, res) {
-	res.json({
-		message: 'NY Times is going Live!!!'
-	})
-});
 
 // This will log http methods and url
 app.use(logger());
 
-//Authentication
-//app.use(authenticate());
 
 // Parser JSON body Requests
 app.use(bodyParser.json());
@@ -51,5 +39,5 @@ app.use(bodyParser.json());
 // We will use the validator after the bodyParser
 app.use(validator());
 
+// Pass app to router
 router(app);
-
